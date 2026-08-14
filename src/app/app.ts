bf1bb9d5dd9a371e93f9c267915fd2f4f55eb9ce
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2026-08-14 19:47:52 -0400

    organizar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from './shared/pages/nav/nav';
+import { Footer } from './shared/pages/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Nav, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('comidasYa');
