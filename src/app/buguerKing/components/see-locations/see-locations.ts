9fb90b3e5398bd5d1af84b7f88d3196ae06e781a
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2026-08-18 17:46:51 -0400

    locations

--- src/app/buguerKing/components/see-locations/see-locations.ts
+++ src/app/buguerKing/components/see-locations/see-locations.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {
+  LucideArrowBigRight,
+  LucideArrowLeft,
+  LucideArrowRight,
+  LucideHouse,
+} from '@lucide/angular';
 
 @Component({
   selector: 'app-see-locations',
-  imports: [],
+  imports: [LucideArrowRight],
   templateUrl: './see-locations.html',
   styleUrl: './see-locations.css',
 })
