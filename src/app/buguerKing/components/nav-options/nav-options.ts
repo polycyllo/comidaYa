62f8725545123997323e64c96d31cedce565246a
Bruno <bruno.salcedo.ca@gmail.com>
2026-08-19 01:33:53 -0400

    feat: add nav-options component and integrate lucide-angular icons in navbar

---
+++ src/app/buguerKing/components/nav-options/nav-options.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { LucideAngularModule, LucideIconData } from 'lucide-angular';
+
+@Component({
+  selector: 'nav-options',
+  imports: [LucideAngularModule],
+  templateUrl: './nav-options.html',
+})
+export class NavOptions {
+
+  label = input.required<string>();
+  icon = input.required<LucideIconData>();
+
+}
--- src/app/shared/pages/nav/nav.ts
+++ src/app/shared/pages/nav/nav.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { NavOptions } from "../../../buguerKing/components/nav-options/nav-options";
+import { ShoppingCart, Search, MenuIcon } from 'lucide-angular/src/icons';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [NavOptions],
   templateUrl: './nav.html',
 })
-export class Nav {}
+export class Nav {
+
+  readonly shoppingCart = ShoppingCart
+  readonly search = Search
+  readonly menuIcon = MenuIcon
+
+}
