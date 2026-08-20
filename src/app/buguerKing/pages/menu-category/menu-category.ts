506c0357916bb6a215403b96ee60a0a2ff2489ea
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2026-08-19 20:50:26 -0400

    fix:make responsive menu-page

--- src/app/buguerKing/pages/menu-category/menu-category.ts
+++ src/app/buguerKing/pages/menu-category/menu-category.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { LucideArrowRight } from '@lucide/angular';
+import { PriceCard } from '../../components/price-card/price-card';
 
 @Component({
   selector: 'app-menu-category',
-  imports: [],
+  imports: [LucideArrowRight, PriceCard],
   templateUrl: './menu-category.html',
   styleUrl: './menu-category.css',
 })
