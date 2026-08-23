b83812a593309b56c697261887c6f5454002ddb4
Bruno <bruno.salcedo.ca@gmail.com>
2026-08-22 21:34:55 -0400

    feat: restructure menu category components and update navigation
    - Added new menu category cards component with HTML and TypeScript files.
    - Updated menu category page to utilize the new menu category cards component.
    - Removed obsolete menu category CSS and spec files.
    - Refactored app structure by moving Nav and Footer components to a shared shell directory.
    - Created new footer and navigation templates with updated HTML structure.
    - Implemented signals for dynamic data handling in menu category and navigation components.
    - Added a new promotional image to the assets.

--- src/app/buguerKing/pages/menu-category/menu-category.ts
+++ src/app/buguerKing/pages/menu-category/menu-category.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MenuCategoryCards } from "../../components/menu-category-cards/menu-category-cards";
+import { MenuItem } from '../../interfaces/MenuItem';
 
 @Component({
   selector: 'app-menu-category',
-  imports: [],
+  imports: [MenuCategoryCards],
   templateUrl: './menu-category.html',
-  styleUrl: './menu-category.css',
 })
-export default class MenuCategory {}
+export default class MenuCategory {
+
+  readonly categories = signal( MENU_CATEGORY_ITEMS)
+
+}
+const MENU_CATEGORY_ITEMS: MenuItem[] = [
+  {
+    title: 'Combos a la Parrilla',
+    img: 'combos-a-la-parrilla.svg',
+  },
+  {
+    title: 'Combos de Pollo',
+    img: 'combos-de-pollo.svg',
+  },
+  {
+    title: 'Hamburguesas Sueltas',
+    img: 'hamburguesas-sueltas.svg',
+  },
+  {
+    title: 'King Jr.',
+    img: 'king-jr.png',
+  },
+  {
+    title: 'King Duo',
+    img: 'king-duo.svg',
+  },
+  {
+    title: 'Acompañamientos',
+    img: 'acompanamientos.svg',
+  },
+  {
+    title: 'Postres',
+    img: 'postres.svg',
+  },
+  {
+    title: 'Ensaladas',
+    img: 'ensaladas.svg',
+  },
+  {
+    title: 'Cupones',
+    img: 'cupones.png',
+  },
+  {
+    title: 'Promociones',
+    img: 'promociones.png',
+  },
+];
